3ea98bc74ea1f391d2fc800cffab14a4d4e2cecc
priyanb <priyanb250@gmail.com>
2026-04-01 19:56:30 +0530

    modified login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { loginUser } from "../api/auth";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await loginUser({ email, password });
+
+      // save token in localStorage
+      localStorage.setItem("token", data.token);
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div style={{ padding: "40px" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /><br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <p>
+        New user? <Link to="/register">Register</Link>
+      </p>
     </div>
   );
 }
