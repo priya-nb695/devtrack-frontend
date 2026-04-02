370eb6b75ecd8ed02e5b2c4697cafd29e51f837a
priyanb <priyanb250@gmail.com>
2026-04-02 21:01:37 +0530

    updated axios page

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,4 +4,11 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+// attach token automatically
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
+
 export default API;
