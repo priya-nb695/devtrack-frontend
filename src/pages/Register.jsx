c9f68865664afa6d700d913944be222422ff59d7
priyanb <priyanb250@gmail.com>
2026-04-02 21:00:19 +0530

    updated register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { registerUser } from "../api/auth";
+
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await registerUser({ name, email, password });
+
+      // auto login after register
+      localStorage.setItem("token", data.token);
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
   return (
-    <div>
-      <h2>Register Page</h2>
+    <div style={{ padding: "40px" }}>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+        /><br/><br/>
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+        /><br/><br/>
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        /><br/><br/>
+
+        <button type="submit">Register</button>
+      </form>
+
+      <p>
+        Already have account? <Link to="/">Login</Link>
+      </p>
     </div>
   );
 }
